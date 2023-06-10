e34a978692c249a13e8937222e8ff35ecfff1bbf
Habid Enrique Manzur Restrepo <habibmanzur@outlook.com>
2023-06-10 01:46:55 -0500

    min fixes

--- backend/src/functions.js
+++ backend/src/functions.js
@@ -1,13 +1,25 @@
-const functions = require("firebase-functions");
+const { onRequest } = require('firebase-functions/v2/https');
 const app = require("./app");
 
-exports.api = functions.runWith({
+// exports.api = functions.runWith({
+//   maxInstances: 10,
+//   secrets: [
+//     "POSTGRESQL_CONNECTION",
+//     "JWT_SECRET_KEY"
+//   ]
+// }).https.onRequest(async (req, res) => {
+//   await require("./utils/postgresql").authenticate();
+//   return app(req, res);
+// });
+
+
+exports.api = onRequest({
   maxInstances: 10,
   secrets: [
     "POSTGRESQL_CONNECTION",
     "JWT_SECRET_KEY"
   ]
-}).https.onRequest(async (req, res) => {
+}, async (req, res) => {
   await require("./utils/postgresql").authenticate();
   return app(req, res);
 });
